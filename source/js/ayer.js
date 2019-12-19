3e1c63f5bd90d333e60e9a74e62ba9a07809bee5
shenyu <448766534@qq.com>
2019-12-19 21:20:41 +0800

    fix: search

--- source/js/ayer.js
+++ source/js/ayer.js
@@ -57,7 +57,7 @@
 
   //
   // 建议在移动端不初始化，其实 /search.xml 文件还挺大的，
-  if ($('.local-search').size() && !isMobile.any()) {
+  if ($('.local-search').size()) {
     $.getScript('/js/search.js', function () {
       searchFunc("/search.xml", 'local-search-input', 'local-search-result');
     });
