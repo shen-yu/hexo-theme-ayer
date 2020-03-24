085292ac1d79fb8fa4dc0556cb26ed944aef1b8c
shenyu <448766534@qq.com>
2020-03-24 19:53:16 +0800

    :rocket: fix: search

--- source-src/js/ayer.js
+++ source-src/js/ayer.js
@@ -30,6 +30,13 @@
       $searchWrap.removeClass('on');
     }
   });
+  
+  // 建议在移动端不初始化，其实 /search.xml 文件还挺大的，
+  if ($('.local-search').size()) {
+    $.getScript('/js/search.js', function () {
+      searchFunc("/search.xml", 'local-search-input', 'local-search-result');
+    });
+  }
 
   // Mobile Detect
   const isMobile = {
